0ea966bfdbc3e264031a7515a816e88d9ecde2da
boutaibissam <isambtb@gmail.com>
2018-09-21 12:05:07 +0100

    built

--- src/main/java/com/bdm/bpmshore/bdm/controller/controller.java
+++ src/main/java/com/bdm/bpmshore/bdm/controller/controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/"bpmshore/spl)
+@RequestMapping("/bpmshore/spl")
 public class controller {
 
     @Autowired

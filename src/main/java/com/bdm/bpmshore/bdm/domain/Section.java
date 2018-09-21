220d7aea45b62dbca8a488af27f56b008956d4ef
boutaibissam <isambtb@gmail.com>
2018-09-21 15:42:20 +0100

    install5

--- src/main/java/com/bdm/bpmshore/bdm/domain/Section.java
+++ src/main/java/com/bdm/bpmshore/bdm/domain/Section.java
@@ -4,14 +4,13 @@ public class Section {
 
     private String rootId;
     private String extension;
-    private Subject subject;
+   // private Subject subject;
 
-    public Section(String rootId, String extension, Subject subject) {
+
+    public Section(String rootId, String extension) {
         this.rootId = rootId;
         this.extension = extension;
-        this.subject = subject;
     }
-
 
     public String getRootId() {
         return rootId;
@@ -27,13 +26,5 @@ public class Section {
 
     public void setExtension(String extension) {
         this.extension = extension;
-    }
-
-    public Subject getSubject() {
-        return subject;
-    }
-
-    public void setSubject(Subject subject) {
-        this.subject = subject;
     }
 }
